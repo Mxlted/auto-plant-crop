f53cc3c53f72831d14aaea22689c6ea4793069cb
Mxlted <nathankumusig@gmail.com>
2025-10-04 15:41:07 -0400

    Updated to 1.21.9

--- src/main/java/tr4nt/autoplantcrops/event/KeyInputHandler.java
+++ src/main/java/tr4nt/autoplantcrops/event/KeyInputHandler.java
@@ -3,31 +3,30 @@ package tr4nt.autoplantcrops.event;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
     public static boolean isOn = true;
-    public static final String AutoPlantCrops_KEY_CATEGORY = "key.category.autoplantcrops";
-    public static final String AutoPlantCrops_KEY_DISABLE = "key.category.autoplantcrops.disablemod";
+
+    // Translation key for the control’s label in Controls UI
+    public static final String KEY_TOGGLE = "key.autoplantcrops.disablemod";
 
     public static KeyBinding disableModKey;
 
-    public static void registerKeyInput()
-    {
-        ClientTickEvents.END_CLIENT_TICK.register(client->
+    public static void registerKeyInput() {
+        ClientTickEvents.END_CLIENT_TICK.register(client ->
                 isOn = !disableModKey.isPressed());
     }
 
-    public static void register()
-    {
-        disableModKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                AutoPlantCrops_KEY_DISABLE,
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_Z,
-                AutoPlantCrops_KEY_CATEGORY
-        ));
+    public static void register() {
+        // Use the constructor that ends with a Category, not a String
+        disableModKey = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        KEY_TOGGLE,           // translation key
+                        GLFW.GLFW_KEY_Z,      // default key
+                        KeyBinding.Category.MISC // pick a built-in category
+                )
+        );
         registerKeyInput();
     }
-
 }
